f809f650664d5e1867b7e3197391cc48b034a60f
Leoma1995 <mannileonardo7@gmail.com>
2022-05-26 17:17:27 +0200

    upd

--- prob_nextState.m
+++ prob_nextState.m
@@ -1,9 +1,9 @@
 
 function prob = prob_nextState(grid)
 
-n_zero = check_value(grid,0);
-if n_zero ~= 0
-    prob = 1/n_zero;
+n_actions = length(free_id(vect_action(grid)));
+if n_actions ~= 0
+    prob = 1/n_actions;
 else
     prob = 0;
 end
